bfd3c718bf3b2f58046c40d4202cfd2701099038
Alberto - Dev Eficiente <alberto@deveficiente.com>
2021-05-26 07:17:58 -0300

    versao inicial do codigo

---
+++ src/main/java/com/deveficiente/desafioencurtadorlink/DesafioEncurtadorLinkApplication.java
@@ -0,0 +1,13 @@
+package com.deveficiente.desafioencurtadorlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEncurtadorLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEncurtadorLinkApplication.class, args);
+	}
+
+}
