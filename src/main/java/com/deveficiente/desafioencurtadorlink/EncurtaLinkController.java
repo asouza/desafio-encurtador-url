323336e7223eee62b7800d64c18e54667223ed02
Alberto - Dev Eficiente <alberto@deveficiente.com>
2021-06-13 11:51:16 -0300

    adequando o codigo para o cdd e ficando com o undertow

--- src/main/java/com/deveficiente/desafioencurtadorlink/EncurtaLinkController.java
+++ src/main/java/com/deveficiente/desafioencurtadorlink/EncurtaLinkController.java
@@ -3,12 +3,13 @@ package com.deveficiente.desafioencurtadorlink;
 import java.net.URI;
 
 import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +21,7 @@ public class EncurtaLinkController {
 	@Autowired
 	private EntityManager manager;
 	@Autowired
-	private GeradorIdLink geradorId;
-	@Autowired
+	//1
 	private TransactionProxy transactionProxy;
 	
 	private static final Logger log = LoggerFactory
@@ -29,16 +29,27 @@ public class EncurtaLinkController {
 
 
 	@PostMapping(value = "/api/encurta")
+	//1
 	public URI executa(@RequestBody @Valid NovoLinkForm request,UriComponentsBuilder uriBuilder) {
 		System.out.println("executando...");
-		LinkEncurtado novoLink = new LinkEncurtado(request.link,geradorId.nextId());
-		transactionProxy.executeInTransaction(() -> {
-			manager.persist(novoLink);			
-		});
-		
-		URI novoRedirecionamento = uriBuilder.path("/{idPublico}").buildAndExpand(novoLink.idPublico).toUri();
-		log.info("[LinkGerado] Novo link gerado {}",novoRedirecionamento);
-		return novoRedirecionamento;
+		//1
+		try {
+			String idLink = RandomStringUtils.randomAlphanumeric(6);
+			//1
+			LinkEncurtado novoLink = new LinkEncurtado(request.link,idLink);
+			//1
+			transactionProxy.executeInTransaction(() -> {
+				manager.persist(novoLink);			
+			});
+			URI novoRedirecionamento = uriBuilder.path("/{idPublico}").buildAndExpand(novoLink.idPublico).toUri();
+			log.info("[LinkGerado] Novo link gerado {}",novoRedirecionamento);
+			return novoRedirecionamento;
+			//1
+		} catch (DataIntegrityViolationException exception) {
+			log.info("[IdDuplicado] Tentando gerar um novo id para links. A tentativa anterior deu errado",exception);
+			//aqui eu poderia ter uma política de retry. Preciso? Será que vou ficar em loop infinito?
+			return executa(request,uriBuilder);
+		}		
 		
 	}
 	
--- src/main/java/com/deveficiente/desafioencurtadorlink/LinkEncurtado.java
+++ src/main/java/com/deveficiente/desafioencurtadorlink/LinkEncurtado.java
@@ -1,8 +1,6 @@
 package com.deveficiente.desafioencurtadorlink;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
@@ -10,17 +8,13 @@ import org.hibernate.validator.constraints.URL;
 
 @Entity
 public class LinkEncurtado {
-
-
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private @NotBlank @URL String linkOriginal;
 	@NotBlank
 	public final String idPublico;
+	private @NotBlank @URL String linkOriginal;
 	
 	
-	public LinkEncurtado(@NotBlank @URL String link, String idPublico) {
+	public LinkEncurtado(@NotBlank @URL String link, @NotBlank String idPublico) {
 		this.linkOriginal = link;
 		this.idPublico = idPublico;
 	}
